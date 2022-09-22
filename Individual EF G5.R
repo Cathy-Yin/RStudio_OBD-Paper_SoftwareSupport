7f2a574152a141504c8026c3d46f3fe22cb0e750
Cathyyy777 <92864318+Cathyyy777@users.noreply.github.com>
2022-09-22 18:45:15 -0500

    Add files via upload

---
+++ Individual EF G5.R
@@ -0,0 +1,131 @@
+library(readr)
+memory.limit(200000)
+
+##Import Data from G6Data folder
+# data1 <- read.csv("Output/G6Data/final2_1-20.csv")
+# data2 <- read.csv("Output/G6Data/final2_21-40.csv")
+# data3 <- read.csv("Output/G6Data/final2_41-53.csv")
+# data <- rbind(data1,data2,data3)
+
+##Import Data from G5Data folder
+data <- data1a
+data$Speed <- round(data$车速/5,0)*5
+rm(data1,data2,data3,data1a)
+VIN <- unique(data$vin)
+
+
+data$SCR上游NOx传感器输出值[is.na(data$SCR上游NOx传感器输出值)] <- 0
+data$SCR下游NOx传感器输出值[is.na(data$SCR下游NOx传感器输出值)] <- 0
+data$SCR监测值 <- data$SCR上游NOx传感器输出值 + data$SCR下游NOx传感器输出值
+
+data_zero <- subset(data, data$车速 <= 0.1)
+data_move <- subset(data, data$车速 > 0.1)
+
+DF_still <- read.csv("Input/UniqueDataTemplate.csv", header = TRUE)
+colnames(DF_still) <- c("N","Engine","Torque")
+DF_move <- read.csv("Input/UniqueDataTemplate2.csv", header = TRUE)
+colnames(DF_move) <- c("N","Engine","Torque","Speed")
+
+summary_still <- data.frame(matrix(ncol = 3, nrow = 0))
+summary_move <- data.frame(matrix(ncol = 3, nrow = 0))
+for (i in 1: length(VIN)){
+    vehiclemove <- data_move[data_move$vin == VIN[i],]
+    vehiclestill <- data_zero[data_zero$vin == VIN[i],]
+    
+    DFU_still <- DF_still
+    DFU_move <- DF_move
+    
+## Find the Drive_count for each Vehicle OBD    
+    stats1 <- data.frame(matrix(ncol = 1, nrow=0))
+    colnames(stats1) <- c("Drive_count")
+    for (j in 1 : nrow(DFU_still)){
+        sub1a <- subset(vehiclestill, vehiclestill$Engine == DFU_still$Engine[j] & vehiclestill$Torque2 == DFU_still$Torque[j]
+                       , select = c(Torque2))
+        sub1b <- nrow(sub1a)
+        stats1 <- rbind(stats1,sub1b)
+    }
+    colnames(stats1) <- c("Drive_count")
+    DFU_still <- cbind(DFU_still, stats1)
+    
+    stats2 <- data.frame(matrix(ncol = 1, nrow=0))
+    colnames(stats2) <- c("Drive_count")
+    for (k in 1 : nrow(DFU_move)){
+        sub2a <- subset(vehiclemove, vehiclemove$Engine == DFU_move$Engine[k] & vehiclemove$Torque2 == DFU_move$Torque[k]
+                       & vehiclemove$Speed == DFU_move$Speed[k], select = c(Torque2))
+        sub2b <- nrow(sub2a)
+        stats2 <- rbind(stats2,sub2b)
+    }
+    colnames(stats2) <- c("Drive_count")
+    DFU_move <- cbind(DFU_move, stats2)
+    message1 <- paste(i, VIN[i],"Operational data count recored.")
+    print(message1)
+## Calculate the valid emission data statistics
+    Ratio_NO_G5 <- exp(2.092286) / (1+exp(2.092286))
+    Ratio_NO2_G5 <- 1 - Ratio_NO_G5
+    NOx_m_G5 <- 30/29 * Ratio_NO_G5 + 46/29 * Ratio_NO2_G5
+
+    vehiclestill <- vehiclestill[(vehiclestill$SCR监测值 <= 3000 & vehiclestill$SCR监测值 >= 1 & vehiclestill$进气量 <= 3212 & vehiclestill$进气量 >= 0 & vehiclestill$发动机燃料流量 <= 100),]
+    vehiclestill <- vehiclestill[!(is.na(vehiclestill$SCR监测值)  | is.na(vehiclestill$进气量)),]
+    vehiclestill$NOx_down_g_s <- vehiclestill$SCR监测值 * (vehiclestill$进气量 + vehiclestill$发动机燃料流量 * 0.85) *0.95 / 3.6 * NOx_m_G5 * 10^-6
+    
+    vehiclemove <- vehiclemove[(vehiclemove$SCR监测值 <= 3000 & vehiclemove$SCR监测值 >= 1 & vehiclemove$进气量 <= 3212 & vehiclemove$进气量 >= 0 & vehiclemove$发动机燃料流量 <= 100),]
+    vehiclemove <- vehiclemove[!(is.na(vehiclemove$SCR监测值)  | is.na(vehiclemove$进气量)),]
+    vehiclemove$NOx_down_g_s <- vehiclemove$SCR监测值 * (vehiclemove$进气量 + vehiclemove$发动机燃料流量 * 0.85) *0.95 / 3.6 * NOx_m_G5 * 10^-6
+
+
+## Find the Emission_count for each Vehicle OBD    
+    stats3 <- data.frame(matrix(ncol = 3, nrow=0))
+    colnames(stats3) <- c("Emission_count","NOx_down_g_s_mean", "NOx_down_g_s_sd")
+    for (l in 1 : nrow(DFU_still)){
+        sub3a <- subset(vehiclestill, vehiclestill$Engine == DFU_still$Engine[l] & vehiclestill$Torque2 == DFU_still$Torque[l]
+                        , select = c(NOx_down_g_s))
+        sub3b <-c(nrow(sub3a), mean(sub3a$NOx_down_g_s), sd(sub3a$NOx_down_g_s))
+        stats3 <- rbind(stats3,sub3b)
+    }
+    colnames(stats3) <- c("Emission_count","NOx_down_g_s_mean", "NOx_down_g_s_sd")
+    DFU_still <- cbind(DFU_still, stats3)
+    
+    stats4 <- data.frame(matrix(ncol = 3, nrow=0))
+    colnames(stats4) <- c("Emission_count","NOx_down_g_s_mean", "NOx_down_g_s_sd")
+    for (m in 1 : nrow(DFU_move)){
+        sub4a <- subset(vehiclemove, vehiclemove$Engine == DFU_move$Engine[m] & vehiclemove$Torque2 == DFU_move$Torque[m]
+                        & vehiclemove$Speed == DFU_move$Speed[m], select = c(NOx_down_g_s))
+        sub4b <- c(nrow(sub4a), mean(sub4a$NOx_down_g_s), sd(sub4a$NOx_down_g_s))
+        stats4 <- rbind(stats4,sub4b)
+    }
+    colnames(stats4) <- c("Emission_count","NOx_down_g_s_mean", "NOx_down_g_s_sd")
+    DFU_move <- cbind(DFU_move, stats4)   
+    
+## Sink the in-motion and immobile datasets
+    write.csv(DFU_still,paste0("C:/Users/admin/OneDrive/文档/Individualstill/G5_",VIN[i],"_G5_still.csv"))
+    write.csv(DFU_move,paste0("C:/Users/admin/OneDrive/文档/Individualmove/",VIN[i],"_G5_move.csv"))
+    message2 <- paste(i,VIN[i],"OBD data has been individually treated.")
+    print(message2)
+    
+    DFU_still$Frequency <- DFU_still$Drive_count / sum(DFU_still$Drive_count)
+    still_NOx_down <- DFU_still$Frequency * DFU_still$NOx_down_g_s_mean
+    still_NOx_down <- sum(still_NOx_down[!is.na(still_NOx_down)])
+    sub_down <- DFU_still[!is.na(DFU_still$NOx_down_g_s_mean),]
+    downratio <- sum(sub_down$Drive_count) / sum(DFU_still$Drive_count)
+
+    vid <- VIN[i]
+    result_still <- c(vid, still_NOx_down, downratio)
+    summary_still <- rbind(summary_still, result_still)
+    
+    DFU_move$Frequency <- DFU_move$Drive_count / sum(DFU_move$Drive_count)
+    move_NOx_down <- DFU_move$Frequency * DFU_move$NOx_down_g_s_mean
+    move_NOx_down <- sum(move_NOx_down[!is.na(move_NOx_down)])
+    sub_down <- DFU_move[!is.na(DFU_move$NOx_down_g_s_mean),]
+    downratio <- sum(sub_down$Drive_count) / sum(DFU_move$Drive_count)
+    
+    result_move <- c(vid, move_NOx_down, downratio)
+    summary_move <- rbind(summary_move, result_move)
+
+    
+    message3 <- paste(i, VIN[i], "data summary recorded")
+    print(message3)
+}
+colnames(summary_move) <- c("VIN","NOx_down","down_ratio")
+colnames(summary_still) <- c("VIN","NOx_down","down_ratio")
+write.csv(summary_still,"C:/Users/admin/OneDrive/文档/Vehicle_G5_still.csv")
+write.csv(summary_move,"C:/Users/admin/OneDrive/文档/Vehicle_G5_move.csv")
